c5e0c031bded723b6bf292dfc6c437b68ee87d52
mmadih <mmadih.216@gmail.com>
2026-04-23 22:59:56 +0200

    Practicing variables,scope,if statement

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+// var x = 5 ; y = 8; z = 20;
+// var sum = x + y + z;
+// console.log("The sum of x, y, and z is: " + sum);
+// y = 6;
+// z = y++;
+// console.log("The new value of y is: " + y);
+// console.log("The new value of z is: " + z);
+// const PI = 3.14;
+// var radius = 4;
+// var area = PI * radius ** 2;
+// console.log("The area of the circle is: " + area);
+// var var1 = 10;
+// var var2 = 20;
+// var temp = var1;
+// var1 = var2;
+// var2 = temp;
+// console.log(var1, var2);
+// var F = 100;
+// var C = (F - 32) * 5 / 9;
+// console.log("The temperature in Celsius is: " + C);
+// {
+//     let x = 10;
+//     console.log("The value of x inside the block is: " + x);
+// }
+let result = 0;     
+for (let i = 0; i < 5; i++) {
+    result += i;
+    console.log("Try " + i + ": The final result is: " + result);
+}
+console.log("The final result is: " + result);
